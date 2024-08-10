3fcab6af1ae45858c72b58997ba1bd83196f2fc3
solly <lssoy77@gmail.com>
2024-08-10 14:50:55 +0900

    init

---
+++ src/pages/Party/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Party = () => {
+  return (
+    <>
+      <div>파티 화면</div>
+    </>
+  );
+};
+
+export default Party;
---
+++ src/pages/Restaurant/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Restaurant = () => {
+  return (
+    <>
+      <div>파티 화면</div>
+    </>
+  );
+};
+
+export default Restaurant;
